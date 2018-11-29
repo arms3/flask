913e3db204e0a945c29f1ce2ac2e55f383ff5ac5
Angus Sinclair <sinclair.angus@gmail.com>
2018-11-29 13:50:36 -0800

    Added display of table
    This reverts commit ee5c97eb39cf179a94bd085afc7b182294bf0a63.

--- fetch_price.py
+++ fetch_price.py
@@ -1,12 +1,22 @@
 import requests
+import requests_cache
 import os
 import pandas as pd
+import io
+
+requests_cache.install_cache()
 
 API_KEY = os.environ.get('QUANDL_API_KEY')
 params = {'api_key': API_KEY, 'ticker': 'AAPL'}
 url = 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES.csv'
-r = requests.get(url, params=params)
-# print(json.loads(r.text))
-# print(r.text)
-df = pd.DataFrame(r.text)
-print(df.head())
+
+
+def fetch():
+    r = requests.get(url, params=params)
+    # print(json.loads(r.text))
+    # print(r.text)
+    df = pd.read_csv(io.StringIO(r.text))
+    # print(df.head())
+    return df.head().to_html()
+
+# fetch()
